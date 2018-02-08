8abb4e0d058b3addf5c43e0088e27fd879c39baa
xieyucheng <848964712@qq.com>
2018-02-08 09:16:12 +0800

    hobbyController接口上传

--- ms_lease_core/src/main/java/com/snowstorms/ms_lease_core/controller/HobbyController.java
+++ ms_lease_core/src/main/java/com/snowstorms/ms_lease_core/controller/HobbyController.java
@@ -1,4 +1,13 @@
 package com.snowstorms.ms_lease_core.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author  xieyucheng
+ * @create  2018/2/8 9:15
+ * @desc   //用户习惯
+ **/
+@RestController
 public class HobbyController {
+
 }
